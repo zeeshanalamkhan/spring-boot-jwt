517c674b943879b5ea4c459466d10f0778da681d
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-25 16:50:31 +0530

    Spring boot startup class added

---
+++ spring-boot-jwts/src/test/java/com/zeeshan/jwt/SpringBootJwtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeshan.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
