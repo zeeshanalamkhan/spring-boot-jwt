9dd38ccbe9e027bfa5e3017b9a4994504c5ef80f
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-25 16:49:12 +0530

    JWTAuthorizationFilter class added

---
+++ spring-boot-jwts/src/main/java/com/zeeshan/jwt/security/JWTAuthorizationFilter.java
@@ -0,0 +1,60 @@
+package com.zeeshan.jwt.security;
+
+import static com.zeeshan.jwt.constants.SecurityConstants.HEADER_STRING;
+import static com.zeeshan.jwt.constants.SecurityConstants.SECRET;
+import static com.zeeshan.jwt.constants.SecurityConstants.TOKEN_PREFIX;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String header = request.getHeader(HEADER_STRING);
+		if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+
+			chain.doFilter(request, response);
+			return;
+		}
+
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+
+		String token = request.getHeader(HEADER_STRING);
+		if (token != null) {
+			String user = JWT
+					.require(Algorithm.HMAC512(SECRET.getBytes()))
+					.build()
+					.verify(token.replace(TOKEN_PREFIX, "")).getSubject();
+			if (user != null) {
+				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+			return null;
+		}
+		return null;
+	}
+}
