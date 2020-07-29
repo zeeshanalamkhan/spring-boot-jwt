f9085c04cac0629a0b37459fecf3ac72ebd24fe6
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-29 08:11:04 +0530

    JwtTokenProvider class

---
+++ spring-boot-jwt/src/main/java/com/zeeshan/jwt/security/JwtTokenProvider.java
@@ -0,0 +1,101 @@
+package com.zeeshan.jwt.security;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import com.zeeshan.jwt.entity.Role;
+import com.zeeshan.jwt.exception.CustomException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenProvider {
+
+	/**
+	 * THIS IS NOT A SECURE PRACTICE! For simplicity, we are storing a static key
+	 * here. Ideally, in a microservices environment, this key would be kept on a
+	 * config-server or store the credentials to Vault Server.
+	 */
+
+	@Value("${security.jwt.token.secret-key:secret-key}")
+	private String secretKey;
+
+	@Value("${security.jwt.token.expire-length:3600000}")
+	private long validityInMilliseconds = 3600000; // 1h
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@PostConstruct
+	public void init() {
+
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+
+	}
+
+	public String createToken(String username, List<Role> roles) {
+
+		Claims claims = Jwts.claims().setSubject(username);
+		claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority()))
+				.filter(Objects::nonNull).collect(Collectors.toList()));
+
+		Date date = new Date();
+		Date validity = new Date(date.getTime() + validityInMilliseconds);
+
+		return Jwts.builder().setClaims(claims).setIssuedAt(date).setExpiration(validity)
+				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+
+	}
+
+	public Authentication getAuthentication(String token) {
+
+		UserDetails userDetails = userDetailsService.loadUserByUsername(getUserName(token));
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+
+	}
+
+	public String getUserName(String token) {
+
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+	}
+
+	public String resolveToken(HttpServletRequest req) {
+
+		String bearerToken = req.getHeader("Authorization");
+
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+
+	public boolean validateToken(String token) {
+
+		try {
+			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException ex) {
+			throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
